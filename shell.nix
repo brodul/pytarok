c86120b41a8a9c76b091ef37094cdf442538a188
Andraz Brodnik <brodul@brodul.org>
2023-04-17 14:41:17 +0200

    Multi python version support

--- shell.nix
+++ shell.nix
@@ -6,8 +6,13 @@ let
   };
 
   dependencies = with pkgs; [
+    stdenv
     python310
+    python311
+    python312
     poetry
+    # python310.pkgs.setuptools
+    python310.pkgs.tox
 
     libffi  # pytest
   ] ++ builtins.getAttr buildType {
@@ -20,8 +25,15 @@ let
 in
 
 pkgs.mkShell {
-  name = "dev-shell";
+  name = "python-env";
   buildInputs = dependencies;
+    # Required for building C extensions
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
   shellHook = ''
+    # rm -rf .nix-shell
+    # mkdir .nix-shell
+    # ln -sf ${pkgs.python310} .nix-shell/python
+    export SOURCE_DATE_EPOCH=315532800
   '';
 }
